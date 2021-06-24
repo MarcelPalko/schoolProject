b1ff457fd4851b7684055895a95b2139918e7501
Marcel Palko <marcel.palko.ext@productoo.com>
2021-06-24 18:29:03 +0200

    Přidání fotek bez pozadí, animační zobrazení tvůrců stránky

---
+++ scripts/app.js
@@ -0,0 +1,36 @@
+// Inicializace obrázků pro rychlost
+window.onload = () => {
+    const profiles = document.getElementById("profiles");
+    const creators = ["david.webp", "dan.webp", "marcel.webp", "dominik.webp"];
+
+    Array.from(profiles.childNodes).filter(item => item.nodeName === "DIV").forEach((element, index) => {
+        const imgSrc = `imgs/${creators[index]}`;
+
+        new Image().src = imgSrc; 
+        element.childNodes[1].src = imgSrc;
+    });
+};
+
+// Zobrazí tvůrce stránky
+const showInfo = () => {
+    const wrapper = document.getElementById("infoWrapper");
+    const profiles = document.getElementById("profiles");
+    wrapper.style.animation = "slide-down 800ms cubic-bezier(0.250, 0.460, 0.450, 0.940) both";
+
+    setTimeout(() => {
+        wrapper.firstElementChild.style.opacity = "1";
+
+        // Nastaví na obrázky animace a efekty
+        Array.from(profiles.childNodes).filter(item => item.nodeName === "DIV").forEach(element => {
+            element.childNodes[1].style.opacity = "1";
+            element.childNodes[3].style.opacity = "1";
+            element.childNodes[1].style.animation = "profiles-slide-down 350ms cubic-bezier(0.25, 0.46, 0.45, 0.94) both";
+        });
+    }, 400);
+};
+
+// Schování dialogu (tvůrců stránky)
+const rollUp = () => {
+    const wrapper = document.getElementById("infoWrapper");
+    wrapper.style.animation = "slide-up 800ms cubic-bezier(0.250, 0.460, 0.450, 0.940) both";
+};
