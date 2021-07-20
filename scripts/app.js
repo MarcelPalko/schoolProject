f1d4f569a48e65eca8993e79677ab007ed981ca3
Marcel Palko <marcel.palko.ext@productoo.com>
2021-07-20 21:58:32 +0200

    Optimalizace stránky #02

--- scripts/app.js
+++ scripts/app.js
@@ -1,9 +1,15 @@
+// Globalní proměnné
+let profiles, infoWrapper;
+
 // Inicializace obrázků pro rychlost
 window.onload = () => {
-    const profiles = Array.from(document.getElementById("profiles").childNodes);
+    profiles = Array.from(document.getElementById("profiles").childNodes);
+    infoWrapper = document.getElementById("infoWrapper");
+
     const pageImgs = Array.from(document.querySelectorAll("img"));
     const creators = ["david.jpg", "dan.jpg", "marcel.jpg", "dominik.jpg"];
 
+    // Načítání fotek u Info Wrapperu
     profiles.filter(item => item.nodeName === "DIV").forEach((element, index) => {
         const imgSrc = `imgs/${creators[index]}`;
 
@@ -26,43 +32,40 @@ window.onload = () => {
                 img.style.opacity = "1";
                 img.style.visibility = "visible";
             }
-        }, 2000);
+        }, 1000);
     });
 };
 
 // Zobrazí tvůrce stránky
 const showInfo = () => {
-    const wrapper = document.getElementById("infoWrapper");
-    const profiles = document.getElementById("profiles");
-    wrapper.style.display = "flex";
-    wrapper.style.animation = "slide-down 800ms cubic-bezier(0.250, 0.460, 0.450, 0.940) both";
+    infoWrapper.style.display = "flex";
+    infoWrapper.style.animation = "slide-down 600ms cubic-bezier(0.250, 0.460, 0.450, 0.940) both";
     document.querySelector("body").style.overflowY = "hidden";
 
     setTimeout(() => {
-        wrapper.firstElementChild.style.opacity = "1";
+        infoWrapper.firstElementChild.style.opacity = "1";
 
         // Nastaví na obrázky animace a efekty
-        Array.from(profiles.childNodes).filter(item => item.nodeName === "DIV").forEach(element => {
+        profiles.filter(item => item.nodeName === "DIV").forEach(element => {
             element.childNodes[1].style.opacity = "1";
             element.childNodes[3].style.opacity = "1";
             element.childNodes[1].style.animation = "profiles-slide-down 350ms cubic-bezier(0.25, 0.46, 0.45, 0.94) both";
         });
-    }, 400);
+    }, 300);
 };
 
 // Schování dialogu (tvůrců stránky)
 const rollUp = () => {
-    const wrapper = document.getElementById("infoWrapper");
-    wrapper.style.animation = "slide-up 800ms cubic-bezier(0.250, 0.460, 0.450, 0.940) both";
+    infoWrapper.style.animation = "slide-up 500ms cubic-bezier(0.250, 0.460, 0.450, 0.940) both";
 
     // Nastaví na obrázky animace a efekty
-    Array.from(profiles.childNodes).filter(item => item.nodeName === "DIV").forEach(element => {
+    profiles.filter(item => item.nodeName === "DIV").forEach(element => {
         element.childNodes[1].style.opacity = "0";
         element.childNodes[3].style.opacity = "0";
     });
 
     setTimeout(() => {
-        wrapper.style.display = "none"
+        infoWrapper.style.display = "none"
         document.querySelector("body").style.overflowY = "auto";
-    }, 900);
+    }, 600);
 };
