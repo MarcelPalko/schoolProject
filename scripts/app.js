cbc1db609a237d8e0c9ddb69b43def3aa535ac27
Marcel Palko <marcelpalko188@gmail.com>
2021-09-05 11:07:34 +0200

    Automatická změna obsahu karet

--- scripts/app.js
+++ scripts/app.js
@@ -1,8 +1,31 @@
 // Globalní proměnné
 let profiles, infoWrapper;
 
+// Enum for ActiveLayout
+const Layouts = Object.freeze({
+    "TwoCards": 2,
+    "ThreeCards": 3
+});
+
+// Settings for page
+let settings = {
+    activeLayout: Layouts.ThreeCards,
+    pageTitle: "",
+    source: "",
+    data: [],
+    generatedNumbers: [],
+    cachedImages: []
+};
+
 // Inicializace obrázků pro rychlost
 window.onload = () => {
+    settings.source = "scripts/data.js";
+
+    if(settings.source) {
+        settings.pageTitle = PAGEDATA[0].pageTitle;
+        settings.data = PAGEDATA[0].data;
+    }
+
     profiles = Array.from(document.getElementById("profiles").childNodes);
     infoWrapper = document.getElementById("infoWrapper");
 
@@ -71,4 +94,76 @@ const rollUp = () => {
         infoWrapper.style.display = "none"
         document.querySelector("body").style.overflowY = "auto";
     }, 600);
+};
+
+// Sets interval for changing layouts and cards content
+setInterval(() => {
+    const cardElements = document.querySelectorAll('.card, .card-reversed');
+
+    setNumbersToSetting();
+    Array.from(cardElements).forEach((card, index) => {
+        setCardData(card, index);
+    });
+}, 9000);
+
+const setNumbersToSetting = () => {
+    settings.generatedNumbers = [];
+    settings.generatedNumbers = [...generateNumbers(settings.activeLayout, 5).map((number) => number === 0 ? 1 : number)];
+};
+
+const generateNumbers = (count, limit) => {
+    const temp = [];
+
+    for (let i = 0; i < (count ? count : 1); i++) {
+        const number = Math.floor(Math.random() * limit);
+        temp.push(number);
+    }
+
+    return temp.length > 1 ? temp : temp[0];
+};
+
+// Choose layout, it depends on the length of quote
+const setLayout = () => {
+    if(settings.data.filter(item => item.quote.length > 60)) {
+        settings.activeLayout = Layouts.TwoCards;
+    }
+};
+
+// Nastavení obsahu karty
+const setCardData = (card, cardIndex) => {
+    Array.from(card.children).forEach((element, index) => {
+        switch(index) {
+            case 0: {
+                const imageSrc = `imgs/profiles/${settings.data[cardIndex]['name'].toLowerCase()}-0${settings.generatedNumbers[index]}.jpg`;
+                
+                element.children[1].src = getCachedImage(imageSrc);
+                break;
+            }
+
+            case 1: {                
+                const quoteNum = generateNumbers(null, settings.data[cardIndex]['quotes'].length);
+
+                element.children[1].innerText = settings.data[cardIndex]['quotes'][+quoteNum];
+                element.children[2].children[0].innerText = settings.data[cardIndex]['name'];
+                element.children[2].children[1].innerText = settings.data[cardIndex]['class'];
+                break;
+            }
+        }
+    });
+};
+
+// Getter for image
+const getCachedImage = (url) => {
+    return editImageCache(url) || settings.cachedImages.find((img) => img.src.includes(url)).src;
+};
+
+// Controls Image Cache
+const editImageCache = (url) => {
+    if(settings.cachedImages.findIndex((img) => img.src.includes(url)) < 0) {
+        let newImage = new Image();
+        newImage.src = url;
+        settings.cachedImages.push(newImage);
+
+        return newImage.src;
+    }
 };
--- scripts/data.js
+++ scripts/data.js
@@ -1,8 +1,29 @@
 const PAGEDATA = [{
     "pageTitle": "Operační Systémy",
     "data": [
-        { "name": "Marcel", "class": "ITA3", "quote": "Programování je jednoduché" },
-        { "name": "Dan", "class": "ITA3", "quote": "Programování mě neba" },
-        { "name": "David", "class": "ITA3", "quote": "Windows 11" }
+        {
+            "name": "Marcel", 
+            "class": "ITA3", 
+            "quotes": [
+                "Virtual box je zajímavý nástroj, dokud se nesetkáte s Windows serverem.",
+                "AAAAA",
+                "aaaaa"
+            ]
+        },
+        { 
+            "name": "Dan", 
+            "class": "ITA3", 
+            "quotes": [ "Packet tracer je zlo =)", "Dan 01" ] 
+        },
+        { 
+            "name": "David", 
+            "class": "ITA3", 
+            "quotes": [ "Myslel jsem si, že věci z hodin v praxi nevyužiji, ale mýlil jsem se.", "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Nullam lectus justo, vulputate eget mollis sed, tempor sed magna. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos hymenaeos. Pellentesque sapien. Aliquam ornare wisi eu metus. Duis sapien nunc, commodo et, interdum suscipit, sollicitudin et, dolor. Nullam justo enim, consectetuer nec, ullamcorper ac, vestibulum in, elit. Maecenas libero. Nunc auctor. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos hymenaeos. Etiam commodo dui eget wisi. Etiam egestas wisi a erat. Duis bibendum, lectus ut viverra rhoncus, dolor nunc faucibus libero, eget facilisis enim ipsum id lacus." ] 
+        },
+        { 
+            "name": "Dominik", 
+            "class": "ITA3", 
+            "quotes": [ "Začátky jsou nudné a máte pocit, že vlastně všechno umíte. Časem se objeví zajímavější, ale těžší věci, které jdou v pohodě zvládnou.", "Dominik 01" ] 
+        },
     ],
 }];
